0587959716581b011e4687f4a34707317b77abdb
dahianamon12 <dahiamonher78@gmail.com>
2026-02-20 09:41:30 -0500

    feat: agregar clase materia y main

---
+++ clases/materias.py
@@ -0,0 +1,7 @@
+class Materia:
+    def __init__(self, codigo, nombre, modalidad, intensidad_horas_sem, duracion_sem):
+        codigo.self = codigo
+        nombre.self = nombre
+        modalidad.self = modalidad
+        intensidad_horas_sem.self = intensidad_horas_sem
+        duracion_sem.self = duracion_sem
